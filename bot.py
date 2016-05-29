235ecd6eccf0ad9f00bf54bb2daaac898494cfd3
johantheolive@gmail.com <russweingarten@gmail.com>
2016-05-29 20:29:27 +0000

    Respond to twitch pings (resolves #1). SIGINT handler to nicely close the irc socket. Utilize twitch api to find channels with most viewers, and connect to them.

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import sys
 import boto3
 import redis
 import ssl
+import signal
 
 BOT_CFG = bot_lib.parse_config() or sys.exit(1)
 
@@ -12,6 +13,14 @@ _redis = redis.StrictRedis(host=BOT_CFG['redis']['endpoint'])
 
 dynamodb = boto3.resource('dynamodb')
 dynamo_table = dynamodb.Table('twitch_messages')
+
+# Get top streams
+twitch = bot_lib.twitch_api()
+
+# Merge channel list from api with configured list
+channel_list = twitch.get_top_stream_list() | BOT_CFG['connection']['channel_list']
+
+print "Connecting to: {}".format(','.join(channel_list))
 
 irc_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc = ssl.wrap_socket(irc_socket)
@@ -22,14 +31,29 @@ irc.send("PASS {}\n".format(BOT_CFG['connection']['password']))
 irc.send("USER {} {} {} :hi\n".format(BOT_CFG['connection']['botnick'], BOT_CFG['connection']['botnick'], BOT_CFG['connection']['botnick']))
 irc.send("NICK {}\n".format(BOT_CFG['connection']['botnick']))
 irc.send("PRIVMSG nickserv :identify {} {}\r\n".format(BOT_CFG['connection']['botnick'], BOT_CFG['connection']['password']))
-irc.send("JOIN {}\n".format(BOT_CFG['connection']['channels']))
+irc.send("JOIN #{}\n".format(',#'.join(channel_list)))
+
+
+def sig_int_handler(signal, frame):
+    print 'SIGINT RECEIVED. CLOSING IRC SOCKET'
+    irc_socket.shutdown(socket.SHUT_RDWR)
+    irc_socket.close()
+    sys.exit()
+
+signal.signal(signal.SIGINT, sig_int_handler)
 
 while True:
-    msg = bot_lib.twitch_message(irc.recv(2040))
+    irc_msg = irc.recv(2040).decode('utf-8')
+
+    # If pinged, then pong back
+    if irc_msg.startswith('PING :tmi.twitch.tv'):
+        irc.send('PONG :tmi.twitch.tv\r\n'.encode())
+
+    msg = bot_lib.twitch_message(irc_msg)
 
     if msg.user_name and msg.message:
         try:
-            print "{} {}: {}".format(msg.channel, msg.user_name, msg.message)
+            print "{}|{}: {}".format(msg.channel, msg.user_name, msg.message)
             _redis.publish(msg.channel, msg.message)
         except UnicodeEncodeError:
             pass
--- bot_lib.py
+++ bot_lib.py
@@ -4,7 +4,9 @@ import time
 import logging
 import requests
 
+
 TWITCH_MSG_REGEX = re.compile(r'^:(?P<user>.+)!.+@.*.tmi.twitch.tv\sPRIVMSG\s#(?P<channel>.+)\s:(?P<msg>.+)', flags=re.M)
+
 
 def __log__(msg):
     logging.info(msg)
@@ -25,7 +27,11 @@ def parse_config(config_file='twitch_irc.cfg'):
             print "Missing config option: {}".format(x)
             return
 
+    cfg['connection']['port'] = int(cfg['connection']['port'])
+    cfg['connection']['channel_list'] = set([x for x in cfg['connection']['channels'].split(',') if x])
+
     return cfg
+
 
 class twitch_api():
     ENDPOINT = 'https://api.twitch.tv/kraken/'
@@ -34,26 +40,30 @@ class twitch_api():
     def __init__(self):
         pass
 
-    def get_channel_list(self, limit=25):
-        url_params = {'limit': limit}
+    def __get(self, url, url_params=None):
         return requests.get(
-                self.ENDPOINT + 'streams',
-                params=url_params,
-                headers=self.HEADERS
-                ).json()
+            self.ENDPOINT + url,
+            params=url_params,
+            headers=self.HEADERS).json()
+
+    def get_top_stream_list(self, limit=25):
+        url_params = {'limit': limit}
+        streams = self.__get('streams', url_params=url_params)['streams']
+        streams = [s['channel']['name'] for s in streams]
+        return set(streams)
 
 
 class twitch_message():
     def __init__(self, msg):
-        self.text = msg.decode('utf-8')
+        self.text = msg.strip()
         self.user_name, self.message, self.channel, self.timestamp = None, None, None, int(time.time())
 
         match = TWITCH_MSG_REGEX.match(self.text)
 
         if match:
-            self.user_name = match.group('user')
-            self.channel = match.group('channel')
-            self.message = match.group('msg')
+            self.user_name = match.group('user').strip()
+            self.channel = match.group('channel').strip()
+            self.message = match.group('msg').strip()
             # self.store_message()
             # self.publish_message()
 
